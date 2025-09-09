05fd6b8dd7cebffc298ae1907cd6f9c6346159d4
Kawã Felipe Ducla Aguiar <kawafelipeducla01@gmail.com>
2025-09-09 11:47:40 -0300

    concluido com sucesso

--- Backend/server.js
+++ Backend/server.js
@@ -1,25 +1,34 @@
 const express = require('express'); // Importa o Express
 const cors = require('cors'); // Importa o CORS
+
+const app = express(); //Cria o servidor
+
+const port = 3000; //Variavel para armazenar a porta
+
 const path = require('path');
 
-const app = express();
-
-// Railway define a porta automaticamente
-const port = process.env.PORT || 3000;
-
-// Para permitir receber JSON nas requisições
+//Para permitir receber json nas requisições
 app.use(express.json());
 app.use(cors());
 
-// Servir arquivos estáticos do Frontend
-app.use(express.static(path.join(__dirname, '../Frontend')));
 
-// Rota principal -> abre o index.html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../Frontend/index.html'));
-});
+//request - requisição
+//response - respota
+app.use(
+     express.static(
+        path.join(__dirname, '../Frontend')
+       )
+    );
 
-// Inicia o servidor
+
+
+
+
+
+
+
+
+
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
-});
+    console.log("Servidor rodando em http://localhost:3000");
+})
